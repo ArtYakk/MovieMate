b1ca52e26a045d18f229cc1dd1a0a4fd5a1e601b
artem <iakonien@gmail.com>
2025-02-11 14:47:23 +0300

    Movie

--- src/main/java/com/artemyakkonen/spring/boot/moviemate/Movie.java
+++ src/main/java/com/artemyakkonen/spring/boot/moviemate/Movie.java
@@ -1,2 +1,109 @@
-package com.artemyakkonen.spring.boot.moviemate;public class Movie {
+package com.artemyakkonen.spring.boot.moviemate;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String author;
+    private String genre;
+    private int year;
+    private String description;
+    private String[] reviews;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String title, String author, String genre, int year, String description) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.year = year;
+        this.description = description;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String[] getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(String[] reviews) {
+        this.reviews = reviews;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", year='" + year + '\'' +
+                ", description='" + description + '\'' +
+                ", reviews=" + Arrays.toString(reviews) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return Objects.equals(title, movie.title) && Objects.equals(author, movie.author) && Objects.equals(genre, movie.genre) && Objects.equals(year, movie.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, genre, year);
+    }
 }
--- src/test/java/com/artemyakkonen/spring/boot/moviemate/MovieJsonTest.java
+++ src/test/java/com/artemyakkonen/spring/boot/moviemate/MovieJsonTest.java
@@ -1,2 +1,34 @@
-package com.artemyakkonen.spring.boot.moviemate;public class MovieJsonTest {
+package com.artemyakkonen.spring.boot.moviemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class MovieJsonTest {
+
+    private final JacksonTester<Movie> json;
+
+    @Autowired
+    public MovieJsonTest(JacksonTester<Movie> json){
+        this.json = json;
+    }
+
+    @Test
+    void myFirstTest(){
+        assertThat(12).isEqualTo(12);
+    }
+
+    @Test
+    void MovieSerializationTest() throws IOException {
+        Movie movie = new Movie(1, "War and Peace", "Lev Tolstoy", "Novel", 1867, "a long story about war and peace");
+        assertThat(json.write(movie)).isStrictlyEqualToJson("expected.json");
+    }
+
+
 }
