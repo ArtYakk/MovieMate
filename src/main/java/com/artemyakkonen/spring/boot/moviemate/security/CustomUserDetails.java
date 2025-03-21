b73cf14519c2b84eb4b29cfdc9d34f72ffdd789b
artem <iakonien@gmail.com>
2025-03-21 08:52:48 +0300

    CustomUserDetails, CustomUserDetailsService

---
+++ src/main/java/com/artemyakkonen/spring/boot/moviemate/security/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.artemyakkonen.spring.boot.moviemate.security;
+
+import com.artemyakkonen.spring.boot.moviemate.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = Arrays.stream(user.getRole().split(", "))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/artemyakkonen/spring/boot/moviemate/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.artemyakkonen.spring.boot.moviemate.security;
+
+import com.artemyakkonen.spring.boot.moviemate.entity.User;
+import com.artemyakkonen.spring.boot.moviemate.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByName(username);
+        CustomUserDetails customUserDetails =  user.map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException(username + "There is no such user in the repository"));
+        return customUserDetails;
+    }
+}
